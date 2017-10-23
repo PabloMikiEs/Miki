d8e5f780c3c44b1b185333d46112f332bb82fd42
miki <pablomiki77@gmail.com>
2017-10-23 22:21:01 +0200

    appointmentsDay

--- public/app/appointmentsDay/appointmentsDay.component.js
+++ public/app/appointmentsDay/appointmentsDay.component.js
@@ -4,22 +4,20 @@ angular.module('appointmentsDayModule', [])
     .component('appointmentsDayrModule', {
         templateUrl:'/app/appointmentsDay/appointmentsday.html',
         controller: function ($scope, $http, $routeParams) {
-        	 
-        	             console.log("Componente appointments day");
-        	 
-        	            if ($routeParams.date) {
-        	 
-        	                 var date = moment($routeParams.date, 'YYYYMM');
-        	                 $scope.year = moment(date).format('YYYY');
-        	                 $scope.month = moment(date).format('MMMM').toUpperCase();
-        	                 date = moment(date).format('YYYYMM');
-        	 
-        	             } else {
-        	 
-        	                 date = moment().startOf('month').format('YYYYMM');
-        	                 $scope.month = moment().startOf('month').format('MMMM').toUpperCase();
-        	               $scope.year = moment().startOf('month').format('YYYY');
-        	 
-        	            }
+
+	            if ($routeParams.date) {
+	 
+	                 var date = moment($routeParams.date, 'YYYYMM');
+	                 $scope.year = moment(date).format('YYYY');
+	                 $scope.month = moment(date).format('MMMM').toUpperCase();
+	                 date = moment(date).format('YYYYMM');
+	 
+	             } else {
+	 
+	                 date = moment().startOf('month').format('YYYYMM');
+	                 $scope.month = moment().startOf('month').format('MMMM').toUpperCase();
+	               $scope.year = moment().startOf('month').format('YYYY');
+	 
+	            }
         	        }
         	    });
